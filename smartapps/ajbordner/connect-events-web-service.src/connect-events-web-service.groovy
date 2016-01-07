58aaafb36392c1b697a2abd8a9af9f02e044b28a
linkAges Connect <innovation@pamf.org>
2016-01-07 13:33:26 -0800

    Added function checkForPresence to remove frequent present/not present transitions in the arrival sensor.

--- smartapps/ajbordner/connect-events-web-service.src/connect-events-web-service.groovy
+++ smartapps/ajbordner/connect-events-web-service.src/connect-events-web-service.groovy
@@ -87,12 +87,27 @@ mappings {
 def listEvents() {
 	def maxNumEvents = 99999
     def startDate = new Date().parse("yyyy-MM-dd", params?.startDate)
-    def endDate = new Date().parse("yyyy-MM-dd", params?.endDate)
- 	def allEvents = contact.eventsBetween(startDate, endDate, [max: maxNumEvents]) + motion.eventsBetween(startDate, endDate, [max: maxNumEvents]) + switches.eventsBetween(startDate, endDate, [max: maxNumEvents])
+    // event data for required sensors
+ 	def allEvents = contact.eventsSince(startDate, [max: maxNumEvents]) + motion.eventsSince(startDate, [max: maxNumEvents]) + switches.eventsSince(startDate, [max: maxNumEvents])
+	// event data for optional sensors
+	if (water != null) {
+    	allEvents = allEvents + water.eventsSince(startDate, [max: maxNumEvents])
+    }
+    if (presence != null) {
+    	allEvents = allEvents + presence.eventsSince(startDate, [max: maxNumEvents])
+    }
    	def resp = []
     allEvents.each { dev ->
     	dev.each {
-    		resp << [label: it.displayName, name: it.name, value: it.value, date: it.isoDate]
+        //log.debug "name = ${it.device?.name}"
+        //log.debug "label = ${it.device?.label}"
+        //log.debug "event_type = ${it?.name}"
+        //log.debug "value = ${it?.value}"
+        //log.debug "date = ${it?.isoDate}"
+        if (it?.name != null) {
+        	resp << [name: it.device?.name, label : it.device?.label, event_type: it?.name, value: it?.value, date: it?.isoDate]
+         }
+    		//resp << [label: it?.displayName, name: it?.name, value: it?.value, date: it?.isoDate]
         }
     }
     return resp
@@ -140,22 +155,23 @@ def initialize() {
     state.powerOn = null
     state.powerChangeTime = (new Date()).time
     state.powerCutoff = 5.0
-    state.activeState = ['Motion Sensor': null, 'Multipurpose A': null, 'Multipurpose B': null]
-    state.changeTime = ['Motion Sensor': (new Date()).time, 'Multipurpose A': (new Date()).time,'Multipurpose B': (new Date()).time]
-    state.lastActivity = ['Motion Sensor': [null,'inactive'], 'Multipurpose A contact': [null,'closed'], 'Multipurpose A acceleration': [null,'active'], 'Multipurpose B contact': [null,'closed'], 'Multipurpose B acceleration': [null,'inactive']]
-    state.delay = ['Motion Sensor': 60, 'Multipurpose A': 60, 'Multipurpose B': 60]
+    state.activeState = ['Motion Sensor': null, 'Multipurpose A': null, 'Multipurpose B': null, 'Arrival Sensor': null]
+    state.changeTime = ['Motion Sensor': (new Date()).time, 'Multipurpose A': (new Date()).time, 'Multipurpose B': (new Date()).time, 'Arrival Sensor': (new Date()).time]
+    state.lastActivity = ['Motion Sensor': [null,'inactive'], 'Multipurpose A contact': [null,'closed'], 'Multipurpose A acceleration': [null,'active'], 'Multipurpose B contact': [null,'closed'], 'Multipurpose B acceleration': [null,'inactive'], 'Arrival Sensor': [null,'not_present']]
+    state.delay = ['Motion Sensor': 60, 'Multipurpose A': 60, 'Multipurpose B': 60, 'Arrival Sensor': 60]
     state.cronMinutes = 1  // every minute
-    schedule("0 0/${state.cronMinutes} * * * ?",postEventsHandler)
+    state.cronSeconds = (new Random()).nextInt(60);
+    schedule("${state.cronSeconds} 0/${state.cronMinutes} * * * ?",postEventsHandler)
 }
 
 def setUpdateInterval() {
   state.cronMinutes = params?.minutes.toInteger()
   unschedule()
-  schedule("0 0/${state.cronMinutes} * * * ?",postEventsHandler)
+  schedule("${state.cronSeconds} 0/${state.cronMinutes} * * * ?",postEventsHandler)
 }
 
 def getUpdateInterval() {
-  return [minutes: state.cronMinutes]
+  return [minutes: state.cronMinutes, seconds: state.cronSeconds]
 }
 
 def addRawEvent(evt) {
@@ -203,6 +219,36 @@ def checkForActivity(sensor, sensorAttribute) {
   }
 }
 
+def checkForPresence() {
+	// Only record presence changes if > state.delay has elapsed. Re-use state variables with "active" <-> "present" and "inactive" <-> "not_present"
+    def now = (new Date()).time
+	def latestActivityState = presence.latestState("presence");
+    def currentActivityState = presence.currentState("presence");
+ 	def latestTime = Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",latestActivityState.dateCreated[0])
+    def timeSinceStateChange = (now - latestTime.time)/1000.0   // time since last state change in seconds
+    def sensorName = presence.name[0]
+    //log.debug "name = ${sensorName}, activeState = ${state.activeState[sensorName]}, timeSinceStateChange = ${timeSinceStateChange}, value = ${currentActivityState.value[0]}"
+    if (state.activeState[sensorName] == null) {
+    	//log.debug "NULL"
+    	state.activeState[sensorName] = (currentActivityState.value[0] == "present")
+        //log.debug "TEST2: ${(currentActivityState.value[0] == "present")}, ${state.activeState[sensorName]}"
+    } else {
+    	if (state.activeState[sensorName] && (currentActivityState.value[0] == "not present") && (timeSinceStateChange > state.delay[sensorName])) {
+        	state.activeState[sensorName] = false
+            def duration = (now - state.changeTime[sensorName])/1000.0 - timeSinceStateChange
+            //log.debug "NOT PRESENT FOR ${duration}"
+            state.changeTime[sensorName] = now
+            state.evtData << [name: sensorName, label: presence.label[0], event_type: sensorAttribute, value: "not present", date: latestActivityState.dateCreated[0], duration: duration.setScale(0, BigDecimal.ROUND_HALF_UP)]
+        } else if (!state.activeState[sensorName] && (currentActivityState.value[0] == "present") && (timeSinceStateChange > state.delay[sensorName])) {
+			state.activeState[sensorName] = true
+            def duration = (now - state.changeTime[sensorName])/1000.0 - timeSinceStateChange
+            //log.debug "PRESENT FOR ${duration}"
+            state.changeTime[sensorName] = now
+            state.evtData << [name: sensorName, label: presence.label[0], event_type: sensorAttribute, value: "present", date: latestActivityState.dateCreated[0], duration: duration.setScale(0, BigDecimal.ROUND_HALF_UP)]
+		}
+    }
+}
+
 def getLatestEventTimes() {
 	def resp = [(switches.name[0]): [switches.latestState("power").dateCreated[0], switches.latestState("power").value[0]]]
     for (dev in state.lastActivity) {
@@ -214,23 +260,30 @@ def getLatestEventTimes() {
 def postEventsHandler() {
 	checkForActivity(motion,"motion")
     checkForActivity(acceleration,"acceleration")
+    if (presence != null) {
+    	checkForPresence()
+     }
     //checkForMotion()
 	// Push if any processed event data
-   	if (state.evtData.size() > 0) {
-		def json = new groovy.json.JsonBuilder(["hub_id": location.hubs[0].id, "location": location.name, "processed": state.evtData, "raw": state.rawEvtData]).toString()
-    	//log.debug("JSON: ${json}")
-    	def params = [
-    		uri: "https://pamf-connect-propane.linkages.org/smartthings.php",
-    	    body: json
-   		]
-    	try {
-        	httpPostJson(params)
-    	} catch ( groovyx.net.http.HttpResponseException ex ) {
-       		log.debug "Unexpected response error: ${ex.statusCode}"
-    	}
-        state.evtData = []   // clear processed event data
-        state.rawEvtData = []  // clear raw event data
-     }
+	def json = new groovy.json.JsonBuilder(["hub_id": location.hubs[0].id, "location": location.name, "processed": state.evtData, "raw": state.rawEvtData]).toString()
+    //log.debug("JSON: ${json}")
+    def params = [
+   		uri: "https://connect.linkages.org/smartthings.php",
+    	body: json
+   	]
+    try {
+        httpPostJson(params)
+    } catch ( groovyx.net.http.HttpResponseException ex ) {
+       	log.debug "Unexpected response error for Connect production: ${ex.statusCode}"
+    }
+    params.uri = "https://pamf-connect-propane.linkages.org/smartthings.php"  // also post data to staging
+    try {
+        httpPostJson(params)
+    } catch ( groovyx.net.http.HttpResponseException ex ) {
+       	log.debug "Unexpected response error for Connect staging: ${ex.statusCode}"
+    }
+    state.evtData = []   // clear processed event data
+    state.rawEvtData = []  // clear raw event data
 }
 
 def switchHandler(evt) {
@@ -284,7 +337,7 @@ def waterHandler(evt) {
 }
 
 def presenceHandler(evt) {
-	addEvent(evt)
+	checkForPresence()
     addRawEvent(evt)
     state.lastActivity[evt.device.name] = [new Date().format("yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone("UTC")), evt.value]
 }
